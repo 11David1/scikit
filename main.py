f07b4786a5d1c5075ec48def0f855c3a5759dd3e
David Mosbach <david@marmos.de>
2026-04-09 13:12:03 +0200

    erkennt fraud

--- main.py
+++ main.py
@@ -6,16 +6,31 @@ from sklearn.model_selection import train_test_split
 clf = RandomForestClassifier(random_state=0)
 
 #feature
+print("load data")
 df = pd.read_csv("creditcard.csv")
+print("data loaded")
 X  = df.iloc[:, :-1] # -> alles, bis auf die letze Spalte
 #classes for feature
 y = df["Class"].values # -> nur die letze Spalte
 
 #data to test
+print("splitting data")
 train_X, test_X, train_y, test_y = train_test_split(X,y, random_state=0)
 
 #learn with the 80%
+print("model training...")
 clf.fit(train_X,train_y)
 
+print("")
+print("Done")
+print("")
+
 #takes the 20% and classifies them
-print(clf.predict(test_X))
+result = clf.predict(test_X)
+
+c = 0
+for i in result:
+    if i == 1:
+        c += 1
+        #print(result[i])
+print(c)
